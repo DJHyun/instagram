3e4e204cce6832a4cba6898b0065b36ede5e0766
DJHyun <djestiny4444@naver.com>
2019-04-10 04:38:03 +0000

    post/create의 textfield 수정

--- posts/forms.py
+++ posts/forms.py
@@ -3,18 +3,17 @@ from .models import Post
 
 
 class PostModelForm(forms.ModelForm):
-    content = forms.CharField(label='content')
+    content = forms.CharField(
+        label='content',
+        widget=forms.Textarea(attrs={
+                'rows' : 5,
+                'cols' : 50,
+                'placeholder' : "지금 뭘 하고 계신가요 ?",
+            })
+            )
     
     class Meta:
         model = Post
         # input을 만들 칼럼 값을 list로 만들어 넣어준다.
         fields = ['content',]
-        widget = {
-            'content': forms.Textarea(attrs={
-                'class': '',
-                'rows' : 5,
-                'cols' : 50,
-                'placeholder' : "지금 뭘 하고 계신가요 ?",
-            })
-        }
         
