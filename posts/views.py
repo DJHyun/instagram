f83fb401be495eb3e4f5ef72846385f459df3bf3
DJHyun <djestiny4444@naver.com>
2019-04-10 05:58:00 +0000

    update 생성

--- posts/views.py
+++ posts/views.py
@@ -15,7 +15,6 @@ def create(request):
             
     # 아니면 (GET 요청이 오면)
     else:
-
         # post를 작성하는 폼을 가져와 template에서 보여줌.
         form = PostModelForm()
         context = {
@@ -35,4 +34,22 @@ def list(request):
 def delete(request, post_id):
     Post.objects.get(pk=post_id).delete()
     return redirect('posts:list')
+
+
+def update(request, post_id):
+    po = Post.objects.get(pk=post_id)
+    if request.method == "POST":
+        # 수정 내용 DB에 반영
+        form = PostModelForm(request.POST,instance=po)
+        if form.is_valid:
+            po = form.save()
+            return redirect('posts:list')
+        pass
+    else:
+        # edit 수정 내용 편집
+        form = PostModelForm(instance=po)
+        context = {
+            'form': form
+        }
+        return render(request, 'posts/update.html',context)
     
