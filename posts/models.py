e187370ec80de65a0e52b63d8eb0f941d93e5df9
DJHyun <djestiny4444@naver.com>
2019-04-16 06:04:57 +0000

    2019-04-16

--- posts/models.py
+++ posts/models.py
@@ -6,8 +6,10 @@ from django.conf import settings
 class Post(models.Model):
     content = models.CharField(max_length=140)
     image = models.ImageField(blank=True)
-    # User와의 YGGR
+    # User와의 YGGR 1 (1:N)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # User와의 YGGR 2 (M:N) Like
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_posts", blank=True)
     
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
