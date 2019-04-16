041a2a9a81c2bc44ea7b8b529b96b62d857c9490
DJHyun <djestiny4444@naver.com>
2019-04-16 07:40:36 +0000

    회원가입, 로그인, 로그아웃 구현

---
+++ accounts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+
+# Create your views here.
+def signup(request):
+    # 회원가입
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('posts:list')
+    else:
+        form = UserCreationForm()
+        return render(request, 'accounts/signup.html', {'form': form})
+        
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:list')
+    
+def login(request):
+    if request.method == 'POST':
+        # 실제 로그인(세션에 유저 정보를 넣는다.)
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request,form.get_user())
+            return redirect('posts:list')
+    else:
+        # 유저로부터 username과 비밀번호를 받는다.
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form':form})
